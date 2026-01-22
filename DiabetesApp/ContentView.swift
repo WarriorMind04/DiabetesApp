1be12c4e8de27f0cd6f88325e339c5d1bacda828
WarriorMind04 <88904471+WarriorMind04@users.noreply.github.com>
2026-01-22 17:12:17 +0100

    Initial Commit

---
+++ DiabetesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiabetesApp
+//
+//  Created by José Miguel Guerrero Jiménez on 22/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiabetesApp/DiabetesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiabetesAppApp.swift
+//  DiabetesApp
+//
+//  Created by José Miguel Guerrero Jiménez on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DiabetesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
