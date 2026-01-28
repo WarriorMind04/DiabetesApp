21a27c6f38b5ef2fe19cff395a6434fe3797046f
WarriorMind04 <88904471+WarriorMind04@users.noreply.github.com>
2026-01-28 09:37:05 +0100

    Apple watch app creation

---
+++ DaibetesWatchApp Watch App/DaibetesWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaibetesWatchAppApp.swift
+//  DaibetesWatchApp Watch App
+//
+//  Created by José Miguel Guerrero Jiménez on 28/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DaibetesWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
